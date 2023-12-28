7c4edffa3e781e7b3e22d261dc1c256d6b3f31ee
nuferron <138325463+nuferron@users.noreply.github.com>
2023-12-28 18:36:15 +0000

    added defines

--- cpp00/ex01/Agenda.h
+++ cpp00/ex01/Agenda.h
@@ -7,13 +7,16 @@
 # include <cstdlib>
 # include <iomanip>
 
-#define RED "\033[1;31m"
-#define GREEN "\033[1;32m"
-#define YELLOW "\033[1;33m"
-#define BLUE "\033[1;34m"
-#define PURPLE "\033[1;35m"
-#define CYAN "\033[1;36m"
-#define WHITE "\033[1;37m"
-#define RESET "\033[0m"
+#define RED         "\033[1;31m"
+#define GREEN       "\033[1;32m"
+#define YELLOW      "\033[1;33m"
+#define BLUE        "\033[1;34m"
+#define PURPLE      "\033[1;35m"
+#define CYAN        "\033[1;36m"
+#define WHITE       "\033[1;37m"
+#define RESET       "\033[0m"
+#define ERR_OPTIONS "The only valid options are: \"ADD\", \"SEARCH\" and \"EXIT\" (this program is case-sensitive)"
+#define EXIT_MSG    "Thank you for using Agenda, see you soon!"
+#define OPTIONS     "\tADD\tSEARCH\t   EXIT"
 
 #endif
