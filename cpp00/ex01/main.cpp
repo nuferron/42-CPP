a2e1805c43ff23c8dbac106fbb2f4c9788f9763e
nuferron <138325463+nuferron@users.noreply.github.com>
2023-12-28 18:40:20 +0000

    added atoi kind of + changed some stuff so it works with invalid input and Ctrl D

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -1,26 +1,56 @@
 #include "Agenda.h"
 #include "PhoneBook.hpp"
 
-/*It takes an integer as user input nd tries to print the contact's information
+/*It takes a string, skips the whitespaces (at the beginning and at the end of the string)
+and returns -1 if the input is invalid and the actual input if it is valid*/
+int search_id(std::string str)
+{
+    int i = 0;
+    int len = str.length() - 1;
+
+    if (str[0] == '-')
+        return (-1);
+    while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t'
+            || str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
+            i++;
+    while (str[len] == ' ' || str[len] == '\n' || str[len] == '\t'
+            || str[len] == '\v' || str[len] == '\f' || str[len] == '\r')
+            len--;
+    while (str[i] == '0')
+        i++;
+    if (len - i > 1)
+        return (-1);
+    if (len - i == -1)
+        return (0);
+    return (str[i] - '0');
+}
+
+/*It takes an integer as user input and tries to print the contact's information
 related to that index. If the input is not an integer, it will display an error message.
 Ctrl-D finishes the program (it's closes fd -> eof).*/
-void    search(PhoneBook &phone_book)
+int    search(PhoneBook &phone_book)
 {
-    int num;
+    std::string str;
+    int         num;
 
+    if (!phone_book.get_total())
+    {
+        std::cout << PURPLE << "There are no contacts to show yet!" << RESET << std::endl;
+        return (0);
+    }
     phone_book.preview_agenda();
-    std::cout << "What contact would you like to see?" << std::endl;
-    std::cin >> num;
+    std::cout << "What contact's details would you like to see?" << std::endl;
+    getline(std::cin, str);
+    num = search_id(str);
     if (std::cin.eof())
-        return ;
+        return (1);
     if (!std::cin)
         std::cout << RED << "Input is not an integer" << RESET << std::endl;
     else if (num < 0 || num >= phone_book.get_total())
         std::cout << RED << "Invalid index" << RESET << std::endl;
     else
         phone_book.agenda[num].print_contact();
-    //std::cin.clear();
-    std::cin.ignore(1, '\n');
+    return (0);
 }
 
 int main(void)
@@ -29,30 +59,25 @@ int main(void)
     std::string  input;
 
     std::cout << "Hi! Try one of these options:" << std::endl;
-    std::cout << "\tADD\tSEARCH\t   EXIT" << std::endl;
+    std::cout << WHITE << OPTIONS << RESET << std::endl;
     while (1)
     {
         getline(std::cin, input);
         if (std::cin.eof())
-            return(0);
-        if (input == "ADD")
-            phone_book.add_contact();
-        else if (input == "SEARCH")
-            search(phone_book);
+            break ;
+        if (input == "ADD" && phone_book.add_contact())
+            break ;
+        else if (input == "SEARCH" && search(phone_book))
+            break ;
         else if (input == "EXIT")
-        {
-            std::cout << GREEN << "Thank you for using Agenda, see you soon!" << RESET << std::endl;
-            return(0);
-        }
-        else
-            std::cout << RED << "The only valid options are: \"ADD\", \"SEARCH\" and \"EXIT\" (this program is case-sensitive)" << RESET << std::endl;
-        if (!std::cin)
-            return(0);
+            return (std::cout << GREEN << EXIT_MSG << std::endl, 0);
         if (input == "ADD" || input == "SEARCH" || input == "EXIT")
         {
             std::cout << std::endl << "What else would you like to do?" << std::endl;
-            std::cout << "\tADD\tSEARCH\t   EXIT" << std::endl;
+            std::cout << WHITE << OPTIONS << RESET << std::endl;
         }
+        else
+            std::cout << RED << ERR_OPTIONS << RESET << std::endl;
     }
     return (1);
 }
