32ff7bbc03a5aa75b2be5176a2fc9e03f8ef1ff5
nuferron <138325463+nuferron@users.noreply.github.com>
2023-12-28 12:23:54 +0000

    finally fully working

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -1,22 +1,26 @@
 #include "Agenda.h"
 #include "PhoneBook.hpp"
 
-int    search(PhoneBook &phone_book)
+/*It takes an integer as user input nd tries to print the contact's information
+related to that index. If the input is not an integer, it will display an error message.
+Ctrl-D finishes the program (it's closes fd -> eof).*/
+void    search(PhoneBook &phone_book)
 {
     int num;
 
     phone_book.preview_agenda();
     std::cout << "What contact would you like to see?" << std::endl;
     std::cin >> num;
+    if (std::cin.eof())
+        return ;
     if (!std::cin)
-        return (1);
+        std::cout << RED << "Input is not an integer" << RESET << std::endl;
     else if (num < 0 || num >= phone_book.get_total())
         std::cout << RED << "Invalid index" << RESET << std::endl;
     else
         phone_book.agenda[num].print_contact();
-    std::cin.clear();
+    //std::cin.clear();
     std::cin.ignore(1, '\n');
-    return (0);
 }
 
 int main(void)
@@ -29,17 +33,12 @@ int main(void)
     while (1)
     {
         getline(std::cin, input);
-        if (!std::cin)
+        if (std::cin.eof())
             return(0);
-        else if (input == "ADD")
-        {
+        if (input == "ADD")
             phone_book.add_contact();
-        }
         else if (input == "SEARCH")
-        {
-            if (search(phone_book))
-                return (1);
-        }
+            search(phone_book);
         else if (input == "EXIT")
         {
             std::cout << GREEN << "Thank you for using Agenda, see you soon!" << RESET << std::endl;
@@ -47,6 +46,8 @@ int main(void)
         }
         else
             std::cout << RED << "The only valid options are: \"ADD\", \"SEARCH\" and \"EXIT\" (this program is case-sensitive)" << RESET << std::endl;
+        if (!std::cin)
+            return(0);
         if (input == "ADD" || input == "SEARCH" || input == "EXIT")
         {
             std::cout << std::endl << "What else would you like to do?" << std::endl;
