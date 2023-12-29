177e5db77c0500c723f92374ef0007cff8b4aa1f
nuferron <138325463+nuferron@users.noreply.github.com>
2023-12-29 22:34:13 +0000

    small modifications, nothing really important

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -4,10 +4,14 @@
 int main()
 {
     std::string str = "HI THIS IS BRAIN";
-    std::string *ptr = &str;
-    std::string &ref = str;
+    std::string *stringPTR = &str;
+    std::string &stringREF = str;
 
-    std::cout << "str MEMORY ADDRESS" << &str << std::endl;
-    std::cout << "memory address HELD by ptr" << ptr << std::endl;
-    std::cout << "memory address HELD by ref" << ref << std::endl;
+    std::cout << "str's MEMORY ADDRESS:\t\t  " << &str << std::endl;
+    std::cout << "memory address HELD by stringPTR: " << stringPTR << std::endl;
+    std::cout << "memory address HELD by stringREF: " << &stringREF << std::endl;
+
+    std::cout << "str CONTENT: " << str << std::endl;
+    std::cout << "ptr CONTENT: " << *stringPTR << std::endl;
+    std::cout << "ref CONTENT: " << stringREF << std::endl;
 }
