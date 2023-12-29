5f74e8e8080357a2daa670113ffc6196b97f5ac6
nuferron <138325463+nuferron@users.noreply.github.com>
2023-12-29 08:35:03 +0000

    upgraded main

--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -3,9 +3,20 @@
 int main()
 {
     Zombie  *saved_zombie;
-
-    saved_zombie = new_zombie("delulu");
-    randomChump("telele");
+    std::string heap_name;
+    std::string stack_name;
+    
+    std::cout << YELLOW << "Insert a name for the zombie saved in the heap memory" << RESET << std::endl;
+    getline(std::cin, heap_name);
+    std::cout << YELLOW << "Insert a name for the zombie saved in the stack memory" << RESET << std::endl;
+    getline(std::cin, stack_name);
+    std::cout << std::endl;
+    if (heap_name == "")
+        heap_name = "HEAP";
+    if (stack_name == "")
+        stack_name = "STACK";
+    saved_zombie = new_zombie(heap_name);
+    randomChump(stack_name);
     saved_zombie->announce();
     delete saved_zombie;
     return (0);
