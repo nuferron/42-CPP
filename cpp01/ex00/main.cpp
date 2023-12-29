be020f8ac2c229c0301642168202be471c6fda53
nuferron <138325463+nuferron@users.noreply.github.com>
2023-12-29 08:52:55 +0000

    cleaned main ft + added protections for user input

--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -1,20 +1,32 @@
 #include "Zombie.h"
 
+static int get_name(std::string &name, std::string mem)
+{
+    std::cout << YELLOW << "Insert a name for the zombie saved in the" + mem + "memory" << RESET << std::endl;
+    getline(std::cin, name);
+    if (std::cin.eof())
+        return (1);
+    if (name == "")
+    {
+        if (mem == "heap")
+            name = "HEAP ZOMBIE";
+        else
+            name = "STACK ZOMBIE";
+    }
+    return (0);
+}
+
 int main()
 {
     Zombie  *saved_zombie;
     std::string heap_name;
     std::string stack_name;
     
-    std::cout << YELLOW << "Insert a name for the zombie saved in the heap memory" << RESET << std::endl;
-    getline(std::cin, heap_name);
-    std::cout << YELLOW << "Insert a name for the zombie saved in the stack memory" << RESET << std::endl;
-    getline(std::cin, stack_name);
+    if (get_name(heap_name, "heap"))
+        return (1);
+    if (get_name(stack_name, "stack"))
+        return (1);
     std::cout << std::endl;
-    if (heap_name == "")
-        heap_name = "HEAP";
-    if (stack_name == "")
-        stack_name = "STACK";
     saved_zombie = new_zombie(heap_name);
     randomChump(stack_name);
     saved_zombie->announce();
