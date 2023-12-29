c7e007d1350a0888aa0ae89c895ee58f0522ffc2
nuferron <138325463+nuferron@users.noreply.github.com>
2023-12-29 08:34:46 +0000

    colors added

--- cpp01/ex00/Zombie.h
+++ cpp01/ex00/Zombie.h
@@ -4,6 +4,15 @@
 #include <iostream>
 #include "Zombie.hpp"
 
+#define RED         "\033[1;31m"
+#define GREEN       "\033[1;32m"
+#define YELLOW      "\033[1;33m"
+#define BLUE        "\033[1;34m"
+#define PURPLE      "\033[1;35m"
+#define CYAN        "\033[1;36m"
+#define WHITE       "\033[1;37m"
+#define RESET       "\033[0m"
+
 Zombie* new_zombie(std::string name);
 void    randomChump(std::string name);
 
