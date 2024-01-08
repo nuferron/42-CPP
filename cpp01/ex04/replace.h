e34755fda4f67894dcb4f452ba6c4543cc553448
nuferron <138325463+nuferron@users.noreply.github.com>
2024-01-08 01:39:01 +0000

    adding header (defines)

---
+++ cpp01/ex04/replace.h
@@ -0,0 +1,16 @@
+#ifndef REPLACE_H
+#define REPLACE_H
+
+#define RED         "\033[1;31m"
+#define GREEN       "\033[1;32m"
+#define YELLOW      "\033[1;33m"
+#define BLUE        "\033[1;34m"
+#define PURPLE      "\033[1;35m"
+#define CYAN        "\033[1;36m"
+#define WHITE       "\033[1;37m"
+#define RESET       "\033[0m"
+
+#define ARGUMENTS   "Invalid arguments! The program takes 3 parameters int he following order: filename string1 string2"
+
+
+#endif
