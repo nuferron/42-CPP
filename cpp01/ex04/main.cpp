deb7d04e387e23cc3b972e7e70d89e93769b11f4
nuferron <138325463+nuferron@users.noreply.github.com>
2024-01-08 01:10:29 +0000

    still working on it, there's something weird about that while

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,27 +1,57 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <stdio.h>
 
-int	main()
+void  write_rep_text(std::string replace, std::string subs, std::string buff, std::ofstream &writeFile)
 {
-	std::ifstream	readFile("test.txt");
-	std::ofstream	writeFile("result.replace");
-	std::string		search;
-	std::string		readout;
+  size_t  found = 0;
+  size_t  i = 0;
+  size_t  len = buff.length();
+  size_t  subs_len = subs.length();
 
-	getline(std::cin, search);
-	std::cout << search;
-    while (getline(readFile, readout)){
-        if(readout == search){
-            /*
-                id remains the same
-                But the First name and Last name are replaced with
-                the user `firstname` and `lastname` input
-            */
-            	std::cout << "aixo es el readout: " << readout << std::endl;
-				writeFile << "HOLA";
+  (void)replace;
+  while (i < len)
+  {
+    found = buff.find(subs, i);
+    //std::cout << "\033[1;31m Busca linia a partir de i: " << i << "\t la troba a "<< found << std::endl;
+    //std::cout << "\033[1;32m Escriura " << buff.substr(i, found - i) << "\n\ti: " << i << " found: " << found << std::endl;
+    writeFile << buff.substr(i, found - i);
+    //writeFile << replace;
+    i += found + subs_len;
+    std::cout << "next: |" << buff[i] << "|" << std::endl;
+    //std::cout << &buff[i] << " len " << subs.length() << std::endl;
+  }
+}
 
-			}
-            std::cout << "readout: " << readout << std::endl;
-    }
+int	main(int argc, char **argv)
+{
+  std::string file;
+  std::string content;
+	std::ifstream	readFile;
+	std::ofstream	writeFile;
+
+  (void)argv;
+  if (argc != 2)
+    return (printf("ARGUMENTS!\n"));
+  file = argv[1];
+  readFile.open(file.c_str());
+  file.resize(file.rfind("."));
+  file.append(".replace");
+  writeFile.open (file.c_str(), std::ofstream::trunc);
+  std::cout << file << std::endl;
+  if (!readFile)
+  {
+    std::cout << "There are problems to read \"" << argv[1] << "\""<< std::endl;
+    return (1);
+  }
+  if (!writeFile)
+  {
+    std::cout << "Sorry, there are problems to create \"" << file << "\""<< std::endl;
+    return (1);
+  }
+  //std::cout << readFile.rdbuf() << std::endl;
+  content.assign( (std::istreambuf_iterator<char>(readFile) ), (std::istreambuf_iterator<char>()    ) );
+  readFile.close();
+  write_rep_text("AAAAAH", "linia", content, writeFile);
 }
