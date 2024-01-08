120917b0ea52cc99179df550837896f961a39a55
Nuria Ferron Ferriz <nuriaf207@gmail.com>
2024-01-08 22:22:01 +0100

    now taking NULLs and their variations 😬

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -1,4 +1,8 @@
 #include "Harl.hpp"
+#include "Harl.h"
+#include <iostream>
+#include <fstream>
+#include <string>
 
 Harl::Harl()
 {
@@ -7,34 +11,56 @@ Harl::Harl()
     _code[2] = "WARNING";
     _code[3] = "ERROR";
     _call[0] = &Harl::_debug;
+    _call[1] = &Harl::_info;
+    _call[2] = &Harl::_warning;
+    _call[3] = &Harl::_error;
 }
 
 Harl::~Harl() {}
 
 void    Harl::_debug()
 {
-    std::cout << "[DEBUG]" << std::endl;
-    std::cout << "6 hours of debugging can save you 5 minutes of reading documentation" << std::endl;
+    std::cout << WHITE << "[DEBUG]" << std::endl;
+    std::cout << PURPLE << "6 hours of debugging can save you 5 minutes of reading documentation" << RESET << std::endl;
 }
 
 void    Harl::_info()
 {
-    std::cout << "I’ve been involved in a number of cults, both a leader and a follower. You have more fun as a follower, but you make more money as a leader." << std::endl;
+	std::cout << WHITE << "[INFO]" << std::endl;
+    std::cout << CYAN << "I’ve been involved in a number of cults, both a leader and a follower. \
+	You have more fun as a follower, but you make more money as a leader." << RESET <<std::endl;
 }
 
 void    Harl::_warning()
 {
-    std::cout << "So, have you tried turning it off and on again?" << std::endl;
+	std::cout << WHITE << "[WARNING]" << std::endl;
+    std::cout << YELLOW << "So, have you tried turning it off and on again?" << RESET << std::endl;
 }
 
 void    Harl::_error()
 {
-    std::cout << "Everything is very bad. Good luck." << std::endl;
+	std::cout << WHITE << "[ERROR]" << std::endl;
+    std::cout << RED << "Everything is very bad. Good luck." << RESET << std::endl;
 }
 
-void    Harl::complain(std::string code)
+void    Harl::complain(std::string level)
 {
-    std::cout << code << std::endl;
-    Harl::_error();
-    _call[0];
+	if (level.empty())
+		return ;
+	for (int i = 0; i < 4; i++)
+    {
+        if (level == Harl::_code[i])
+        {
+            (this->*_call[i])();
+            break ;
+        }
+    }
+}
+
+void	Harl::complain(const void *ptr)
+{
+	if (!ptr)
+		return ;
+	else
+		this->complain(std::string((char *)ptr));
 }
--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -5,4 +5,9 @@ int main(void)
     Harl    msg;
 
     msg.complain("ERROR");
+    msg.complain("");
+    msg.complain((void *)0);
+    msg.complain(NULL);
+    msg.complain("WARNING");
+    msg.complain("INFO");
 }
