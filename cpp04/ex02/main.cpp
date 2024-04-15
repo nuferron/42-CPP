ff24ef1e755456ba59bca3aed6fe4c8dd45eafb2
Nuria Ferron Ferriz <nuriaf207@gmail.com>
2024-04-15 19:02:06 +0200

    improved main + added not compiling main test (abstract classes can't be instantiable)

--- cpp04/ex02/main.cpp
+++ cpp04/ex02/main.cpp
@@ -3,19 +3,33 @@
 
 int main()
 {
-    Cat cat;
-
-    std::cout << "Creating cat2" << std::endl << std::endl;
-
-    Cat cat2(cat);
-    Dog dog;
-
-    cat.makeSound();
-    cat.setIdea("hello");
-    cat.printIdeas();
-
-    std::cout << std::endl << std::endl;
-    cat2.printIdeas();
-    dog.makeSound();
-    dog.printIdeas();
+	Cat cat;
+	Cat	cat1(cat);
+	Dog dog;
+	
+	std::cout << std::endl << "CAT" << std::endl;
+	cat.makeSound();
+	cat.setIdea("I'm hungry");
+	cat.printIdeas();
+	
+	std::cout << std::endl << "CAT1" << std::endl;
+	cat1.makeSound();
+	cat1.printIdeas();
+	
+	std::cout << std::endl << "CAT2" << std::endl;
+	Cat cat2(cat);
+	cat2.printIdeas();
+	
+	std::cout << std::endl << "DOG" << std::endl;
+	dog.makeSound();
+	dog.printIdeas();
+	std::cout << std::endl;
+	return (0);
 }
+
+/*int main() // Does NOT compile (it shouldn't)
+{
+	AAnimal animal;
+
+	(void)animal;
+}*/
