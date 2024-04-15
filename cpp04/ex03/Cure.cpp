a0b44589cf43d7b78eb16e06cfa95e827f8326c9
Nuria Ferron Ferriz <nuriaf207@gmail.com>
2024-04-15 19:07:27 +0200

    changed print according to subject

--- cpp04/ex03/Cure.cpp
+++ cpp04/ex03/Cure.cpp
@@ -21,5 +21,5 @@ Cure *Cure::clone() const
 
 void    Cure::use(ICharacter &target)
 {
-    std::cout << " heals " << target.getName() << "'s wounds" << std::endl;
+    std::cout << "* heals " << target.getName() << "'s wounds *" << std::endl;
 }
--- cpp04/ex03/Ice.cpp
+++ cpp04/ex03/Ice.cpp
@@ -19,5 +19,5 @@ Ice *Ice::clone() const
 
 void    Ice::use(ICharacter &target)
 {
-    std::cout << " shoots an ice bolt at " << target.getName() << std::endl;
+    std::cout << "* shoots an ice bolt at " << target.getName()  << " *"<< std::endl;
 }
