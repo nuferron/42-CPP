520e22539c237e3a6353edb91143a09cead4a012
Nuria Ferron Ferriz <nuriaf207@gmail.com>
2024-04-16 22:34:19 +0200

    added tests

--- cpp05/ex02/main.cpp
+++ cpp05/ex02/main.cpp
@@ -6,11 +6,13 @@
 /*   By: nuferron <nuferron@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 16:11:31 by nuferron          #+#    #+#             */
-/*   Updated: 2024/04/16 21:40:43 by nuferron         ###   ########.fr       */
+/*   Updated: 2024/04/16 22:33:31 by nuferron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
 
 void	test_shrub()
 {
@@ -22,12 +24,52 @@ void	test_shrub()
 	b.executeForm(s);
 }
 
+void	test_robot()
+{
+	RobotomyRequestForm	r;
+	Bureaucrat			b("Manfred", 45);
+	b.signForm(r);
+	std::cout << r.getTarget() << std::endl;
+	//r.execute(b);
+	b.executeForm(r);
+	b.executeForm(r);
+	b.executeForm(r);
+}
+
+void	test_pardon()
+{
+	PresidentialPardonForm	p;
+	Bureaucrat				b("Grisham", 5);
+
+	b.signForm(p);
+	//p.execute(b);
+	b.executeForm(p);
+}
+
 int main()
 {
 	try
 	{
 		std::cout << "TESTING SHRUBBERY" << std::endl;
 		test_shrub();
+	}
+	catch (std::exception &ex)
+	{
+		std::cout << ex.what() << std::endl;
+	}
+	try
+	{
+		std::cout << std::endl << "TESTING ROBOTOMY" << std::endl;
+		test_robot();
+	}
+	catch (std::exception &ex)
+	{
+		std::cout << ex.what() << std::endl;
+	}
+	try
+	{
+		std::cout << std::endl << "TESTING PARDON" << std::endl;
+		test_pardon();
 	}
 	catch (std::exception &ex)
 	{
