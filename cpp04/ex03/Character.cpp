55bc841c30566cf6ce8c091d4fa0048fbc1b7656
Nuria Ferron Ferriz <nuriaf207@gmail.com>
2024-04-16 13:39:42 +0200

    added discard pile management

--- cpp04/ex03/Character.cpp
+++ cpp04/ex03/Character.cpp
@@ -2,18 +2,21 @@
 
 Character::Character(): _total(0), _name("Hans")
 {
+	this->_discarded = NULL;
     for (int i = 0; i < 4; i++)
         this->_slots[i] = NULL;
 }
 
 Character::Character(const std::string &name): _total(0), _name(name)
 {
+	this->_discarded = NULL;
     for (int i = 0; i < 4; i++)
         this->_slots[i] = NULL;
 }
 
 Character::Character(Character &c): _total(c._total), _name(c._name)
 {
+	this->_discarded = NULL;
     for (int i = 0; i < 4; i++)
     {
         if (this->_slots[i])
@@ -32,12 +35,25 @@ Character::~Character()
         if (this->_slots[i])
             delete this->_slots[i];
     }
+	if (this->_discarded)
+	{
+		for (int i = 0; this->_discarded[i]; i++)
+			delete this->_discarded[i];
+		delete [] this->_discarded;
+	}
 }
 
 Character &Character::operator=(const Character &c)
 {
     this->_total = c._total;
     this->_name = c._name;
+	if (this->_discarded)
+	{
+		for (int i = 0; this->_discarded[i]; i++)
+			delete this->_discarded[i];
+		delete [] this->_discarded;
+	}
+	this->_discarded = NULL;
     for (int i = 0; i < 4; i++)
     {
         if (this->_slots[i])
@@ -59,6 +75,11 @@ void    Character::equip(AMateria *m)
 {
     int empty = -1;
 
+	if (!m)
+	{
+		std::cout << "Nothing to equip!" << std::endl;
+		return ;
+	}
     if (this->_total == 4)
     {
         std::cout << "Full inventory!" << std::endl;
@@ -75,7 +96,7 @@ void    Character::equip(AMateria *m)
         }
     }
     this->_slots[empty] = m;
-    std::cout << m->getType() << " has been equiped in slot " << empty << std::endl;
+    //std::cout << m->getType() << " has been equiped in slot " << empty << std::endl;
     this->_total++;
 }
 
@@ -92,6 +113,7 @@ void    Character::unequip(int idx)
         return ;
     }
     std::cout << this->_slots[idx]->getType() << " has been unequipped" << std::endl;
+	this->_discarded = this->_toDiscardPile(this->_slots[idx]);
     this->_slots[idx] = NULL;
     this->_total--;
 }
@@ -104,4 +126,23 @@ void    Character::use(int idx, ICharacter &target)
         return ;
     }
     this->_slots[idx]->use(target);
+}
+
+AMateria	**Character::_toDiscardPile(AMateria *m)
+{
+	int total = 0;
+	AMateria **newpile;
+
+	if (this->_discarded)
+	{	
+		while (this->_discarded[total])
+			total++;
+	}
+	newpile = new AMateria*[total + 2];
+	for (int i = 0; i < total; i++)
+		newpile[i] = this->_discarded[i];
+	newpile[total] = m;
+	newpile[total + 1] = NULL;
+	delete [] this->_discarded;
+	return (newpile);
 }
