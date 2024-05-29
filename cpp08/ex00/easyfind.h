27169cdb09272c7adb0f232113fb96de17508936
Nuria Ferron Ferriz <nuriaf207@gmail.com>
2024-05-29 12:57:00 +0200

    not using iterators

--- cpp08/ex00/easyfind.h
+++ cpp08/ex00/easyfind.h
@@ -3,18 +3,12 @@
 
 #include <iostream>
 
-template <typename T> bool  easyfind(T &var, int num)
+template <typename T>
+bool easyfind(T &var, int num)
 {
-    typename T::const_iterator it = var.begin();
-    typename T::const_iterator ite = var.end();
-
-    while (it != ite)
-    {
-        if (*it == num)
-            return (1);
-        it++;
-    }
-    return (0);
+	if (std::find(var.begin(), var.end(), num) != var.end())
+		return (true);
+	return (false);
 }
 
 #endif
